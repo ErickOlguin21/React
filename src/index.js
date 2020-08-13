d5913ccf8d0359d8c25713c2345ea194b03fcd78
Erick_Olguin <erick@sipges.com>
2020-08-13 12:37:39 -0500

    Se añadieron componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/Card'
+import Welcome from './components/Welcome'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<div>
+    <Welcome 
+    username = 'Erick'
+    />
+    <Card
+        title = 'Technique Guides'
+        description = 'Learn amazing street workout and calisthenics'
+        img = ''
+        leftColor = '#A74CF2'
+        rightColor = '#617BFB'
+    />
+</div>, container )
+
+// const user = {
+//     firstName: 'Erick',
+//     lastName: 'Olguin',
+//     avatar: 'http'
+// }
+// function getName(user){
+//     return `${user.firstName} ${user.lastName}`
+// }
+
+// function getGreeting(user){
+//     if(user){
+//         return <h1>Hello {getName(user)}</h1>
+//     }
+//     return <h1>Hello Stranger</h1>
+// }
+
+// const name = 'Erick'
+// // const element = <img src= {user.avatar} />
+// const element = (
+//     <div>
+//         <h1>{getGreeting(user)}</h1>
+//         <img src= {user.avatar} />
+//     </div>
+//     )
+// const container = document.getElementById('root')
+
+// // ReactDOM.render(__QUE__,__DONDE__)
+
+// ReactDOM.render(element,container)
